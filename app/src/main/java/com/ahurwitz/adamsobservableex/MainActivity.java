71006d50a6dbce0b254706d1a64b3a10421c9d6b
Adam Hurwitz <adam@close5.com>
2016-05-05 19:56:01 -0700

    feat: transform ArrayLits with .map() Operator

--- app/src/main/java/com/ahurwitz/adamsobservableex/MainActivity.java
+++ app/src/main/java/com/ahurwitz/adamsobservableex/MainActivity.java
@@ -1,19 +1,17 @@
 package com.ahurwitz.adamsobservableex;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Switch;
-import android.widget.Toast;
+import android.widget.TextView;
 
 
 import java.util.ArrayList;
-import java.util.Observable;
 
+import rx.Observable;
 import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,15 +22,12 @@ public class MainActivity extends AppCompatActivity {
     Object obj3 = new Object(3, "obj3", true);
     Object obj4 = new Object(4, "obj4", false);
     Object obj5 = new Object(5, "obj5", true);
-    rx.Observable observable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Switch switchBtn = (Switch) findViewById(R.id.boolean_switch_btn);
-        //Button filterListBtn = (Button) findViewById(R.id.filterListBtn);
 
         objects.add(obj1);
         objects.add(obj2);
@@ -40,29 +35,64 @@ public class MainActivity extends AppCompatActivity {
         objects.add(obj4);
         objects.add(obj5);
 
+        TextView obj1View = (TextView) findViewById(R.id.obj1_id);
+        TextView obj2View = (TextView) findViewById(R.id.obj2_id);
+        TextView obj3View = (TextView) findViewById(R.id.obj3_id);
+        TextView obj4View = (TextView) findViewById(R.id.obj4_id);
+        TextView obj5View = (TextView) findViewById(R.id.obj5_id);
+
+        obj1View.setText(String.valueOf(obj1.getVal()));
+        obj2View.setText(String.valueOf(obj2.getVal()));
+        obj3View.setText(String.valueOf(obj3.getVal()));
+        obj4View.setText(String.valueOf(obj4.getVal()));
+        obj5View.setText(String.valueOf(obj5.getVal()));
+
+        Switch switchBtn = (Switch) findViewById(R.id.boolean_switch_btn);
+
+        /*observable.filter(new Func1() {
+            @Override
+            public java.lang.Object call(java.lang.Object o) {
+                return null;
+            }
+        });*/
+
         switchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               observable.map(filterList(objects));
                 if (switchBtn.isChecked()) {
-                    Log.v(LOG_TAG, "Filter Observable");
-                    //TODO: Create observable and pass in ArrayList of Objects in .map() filter,
-                    // print out objects with values
+                    Observable myObservable = Observable.from(objects);
+                    /*myObservable.map(filterList())
+                            .toList()
+                            .toBlocking()
+                            .single();*/
+                    Log.v(LOG_TAG, "Transform Observable: " +
+                            myObservable.map(filterList())
+                                    .toList()
+                                    .toBlocking()
+                                    .single()
+                            //TODO: attach tramsformed list to something
+                    );
                 }
             }
         });
     }
 
     @NonNull
-    private Func1<ArrayList<Object>, ArrayList<Object>> filterList(ArrayList<Object> objects) {
-        return filteredObjects -> {
-            filteredObjects = objects;
-            return Observable
-                    .map(filterList(filteredObjects))
-                    .toList()
-                    .toBlocking()
-                    .single();
+    private Func1<Object, Object> filterList() {
+        return filteredObject -> {
+            filteredObject.setVal(true);
+            return filteredObject;
         };
     }
 
 }
+
+   /*public Action1<Object> getAction()
+    {
+        return new Action1<Object>() {
+            @Override
+            public void call(Object object) {
+
+            }
+        };
+    }*/
